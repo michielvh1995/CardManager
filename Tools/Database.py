c5b148d1b5ffcf96e8d759888894d3fda5ccf52d
Michiel <michielvh1995@gmail.com>
2020-01-29 17:52:04 +0100

    Updated Readme TODO and allowed to insert values per field

--- Tools/Database.py
+++ Tools/Database.py
@@ -20,6 +20,11 @@ class SQLInterpreter:
         output:
                     (boolean, error):   whether or not the query was successfully executed
         """
+        print "# DEBUG: "
+        print sql
+        print
+        print self.re_insert.match(sql)
+
 
         # TODO: change the field and value items to lists
         out =["INSERT",None,None,None,None]
@@ -35,81 +40,105 @@ class SQLInterpreter:
 # ------------------------------------------------------------
 
 class Table:
-    def __init__(self, fields, where=None):
-        self.fields = fields
-        self.header = {}
-        self.rows = []
+    def __init__(self, fieldnames, where=None):
+        self.fields = fieldnames    #
+        self.header = []        # The field names
+        self.rows = []          # [{name: field}]
 
-        for i, f in enumerate(fields):
-            self.header[f] = i
+        for f in self.fields:
+            self.header.append(f)
 
-
-    def Insert(self, fields, values, ret = None):
+    def Insert(self, names, values, ret = None):
+        """ Insert a new row to the table
         """
-            Insert a new row to the table
-        """
-        row = [None]*len(self.fields)
-
-        for i, f in enumerate(fields):
-            row[self.header[f]] = values[i]
+        row = {}
+        for i, f in enumerate(names):
+            row[f] = values[i]
 
         self.rows.append(row)
 
         if ret:
             return "asd"
 
+class Card:
+    def __init__(self):
+        self.id = None
+
 class DataBase:
     def __init__(self, passwd = None):
-        self.sqlinterpreter = SQLInterpreter()
+        # Necessary?
+        self.sqlinterpreter = SQLInterpreter()      # Cleaning SQL and performing actions (?)
+        self.tablenames = []                        # List of the table names
+        self.tables = {}                            # { name : Table }
 
-        # TODO: create table creation logic
-        self.tables = []
-        self.tblnames = {}
-
-
-        self.tables.append(Table(["id"]))
-        self.tblnames["test"] = 0
-
-        if passwd:
-            # Encryption logic
-            self.enc = True
+    def create_table(self, name, fields, where = None, ret = None):
+        """ Create and add a new table to the database
+        """
+        tbl = Table(fields)
+        self.tables[name] = tbl
+        self.tablenames.append(name)
+    #
 
     def Query(self, query):
         """ Execute a query """
         # Interpret SQL
+        # Currently hardcoded to just accapt insertion queries
         act, tb, fields, values, ret = self.sqlinterpreter.Match(query)
+        fields = fields.split(",")
+        values = values.split(",")
 
+        print fields
         if act == "INSERT":
-            ret = self.insert(tb, [fields], [values], ret)
+            ret = self.insert(tb, fields, values, ret)
 
 
         return ret
 
     def insert(self, tbl, fields, values, ret):
+        """ Insert a row into a table
         """
-            Insert a row into a table
-        """
-        print "DB.insert"
-        return self.tables[self.tblnames[tbl]].Insert(fields, values, ret)
+        if tbl in self.tablenames:
+            self.tables[tbl].Insert(fields, values)
 
-    def create_table(self, name, fields, where = None, ret = None):
-        """
+            # Query return values and return them
 
-        """
-        print "create table"
-        self.tables.append(Table(fields, where))
-        self.tblnames[name] = len(self.tables)
+            return "TRUE"
+        else:
+            return "ERROR"
+    #
+    # def create_table(self, name, fields, where = None, ret = None):
+    #     """
+    #
+    #     """
+    #     print "create table"
+    #     self.tables.append(Table(fields, where))
+    #     self.tblnames[name] = len(self.tables)
+    #
+    #     return ret
 
-        return ret
 
-
-
-
-
+# ===================================================
+# Debugging
+# ===================================================
+print "init \n"
 db = DataBase()
 
-db.Query("INSERT INTO test (id) VALUES (1);")
-print db.tables[0].rows
+print "create table \n"
+db.create_table("test", fields = ["id", "value"])
+
+print "tables"
+print db.tables["test"]
+
+print "query"
+print db.Query("INSERT INTO test (id) VALUES (1);")
+print
+
+print ("queried")
+print db.tables["test"].rows
+print
 
 db.create_table("test2", ["id", "test", "val"])
-db.Query('INSERT INTO test2 ("test", "val", "id") VALUES ("AAA", "BBB", "0")')
+print db.Query('INSERT INTO test2 ("test", "val", "id") VALUES ("AAA", "BBB", "0");')
+
+
+print db.tables["test2"].rows
--- readme.md
+++ readme.md
@@ -13,3 +13,22 @@ The UI will be provided by the tkinter package
 idk yet
 
 Encrypted skiplist & hashtable
+
+
+
+# TODO:
+* Rewrite database logic in C/C++
+
+## Python TODO:
+### Tier 1:
+  * Allow for return values for queries
+  * Allow for searching for a value for a field
+  * Allow for subsetting on rows
+  * Save databases/tables on the harddrive
+  * Load databases from a file
+
+### Tier 2:
+  * Make it into a deamon
+
+### Tier 3:
+  * Encryption
